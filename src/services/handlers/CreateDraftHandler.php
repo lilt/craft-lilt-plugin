0c698ae2adbe5884b5b63c46d772923a36fd4fe2
Volodymyr Hadomskyi <gadomskyy.v@gmail.com>
2022-08-02 21:54:45 +0200

    fix: - css styles - wrong mapping for fields - canceled verified job gets wrong status

--- src/services/handlers/CreateDraftHandler.php
+++ src/services/handlers/CreateDraftHandler.php
@@ -9,6 +9,7 @@ use craft\base\ElementInterface;
 use craft\errors\ElementNotFoundException;
 use lilthq\craftliltplugin\Craftliltplugin;
 use lilthq\craftliltplugin\datetime\DateTime;
+use lilthq\craftliltplugin\parameters\CraftliltpluginParameters;
 use Throwable;
 use yii\base\Exception;
 
@@ -19,10 +20,20 @@ class CreateDraftHandler
      * @throws Throwable
      * @throws ElementNotFoundException
      */
-    public function create(ElementInterface $element, string $jobTitle, int $sourceSiteId, int $targetLanguage): ElementInterface
-    {
+    public function create(
+        ElementInterface $element,
+        string $jobTitle,
+        int $sourceSiteId,
+        int $targetSiteId
+    ): ElementInterface {
+
+        /** Element will be created from original one, we can't create draft from draft */
+        $createFrom = $element ? Craft::$app->elements->getElementById(
+            $element->getCanonicalId()
+        ) : $element;
+
         $draft = Craft::$app->drafts->createDraft(
-            $element,
+            $createFrom,
             Craft::$app->user->getId(),
             sprintf(
                 '%s [%s -> %s] ' . (new DateTime())->format('H:i:s'),
@@ -31,10 +42,16 @@ class CreateDraftHandler
                     $sourceSiteId
                 ),
                 Craftliltplugin::getInstance()->languageMapper->getLanguageBySiteId(
-                    $targetLanguage
+                    $targetSiteId
                 )
             ),
             null
+        );
+
+        $draft = Craft::$app->elements->getElementById(
+            $draft->getId(),
+            'craft\elements\Entry',
+            $targetSiteId
         );
 
         $fieldLayout = $element->getFieldLayout();
@@ -43,10 +60,24 @@ class CreateDraftHandler
         foreach ($fields as $field) {
             $field->copyValue($element, $draft);
         }
-        Craft::$app->elements->saveElement($draft);
+        /*
+        if(
+            get_class($field) === CraftliltpluginParameters::BENF_NEO_FIELD
+            || get_class($field) === CraftliltpluginParameters::CRAFT_FIELDS_MATRIX
+            || get_class($field) === CraftliltpluginParameters::CRAFT_FIELDS_SUPER_TABLE
+        ) {
+            $draft->setFieldValue($field->handle, $element->getFieldValue($field->handle));
+        }
+        */
+        //Craft::$app->elements->saveElement($draft);
 
+        $draft->title = $element->title;
+
+        $draft->duplicateOf = $element;
         $draft->mergingCanonicalChanges = true;
         $draft->afterPropagate(false);
+
+        Craft::$app->elements->saveElement($draft);
 
         return $draft;
     }
