a3a96718a4cff0939fcb9360fdf729f840ff8ad0
Volodymyr Hadomskyi <volodymyr.hadomskyi@outlook.com>
2023-01-17 21:18:21 +0100

    Fix FetchJobStatusFromConnector assertions

--- tests/integration/modules/FetchJobStatusFromConnectorCest.php
+++ tests/integration/modules/FetchJobStatusFromConnectorCest.php
@@ -6,6 +6,7 @@ namespace lilthq\craftliltplugintests\integration\modules;
 
 use Codeception\Exception\ModuleException;
 use Craft;
+use craft\elements\Entry;
 use craft\helpers\Db;
 use IntegrationTester;
 use LiltConnectorSDK\Model\JobResponse;
@@ -13,13 +14,24 @@ use LiltConnectorSDK\Model\SettingsResponse;
 use lilthq\craftliltplugin\Craftliltplugin;
 use lilthq\craftliltplugin\modules\FetchInstantJobTranslationsFromConnector;
 use lilthq\craftliltplugin\modules\FetchJobStatusFromConnector;
+use lilthq\craftliltplugin\modules\FetchTranslationFromConnector;
 use lilthq\craftliltplugin\modules\FetchVerifiedJobTranslationsFromConnector;
 use lilthq\craftliltplugintests\integration\AbstractIntegrationCest;
+use lilthq\tests\fixtures\EntriesFixture;
 use PHPUnit\Framework\Assert;
 use yii\db\Exception;
 
 class FetchJobStatusFromConnectorCest extends AbstractIntegrationCest
 {
+    public function _fixtures(): array
+    {
+        return [
+            'entries' => [
+                'class' => EntriesFixture::class,
+            ]
+        ];
+    }
+
     /**
      * @throws Exception
      * @throws ModuleException
@@ -31,9 +43,14 @@ class FetchJobStatusFromConnectorCest extends AbstractIntegrationCest
         $user = Craft::$app->getUsers()->getUserById(1);
         $I->amLoggedInAs($user);
 
-        $job = $I->createJob([
+        $element = Entry::find()
+            ->where(['authorId' => 1])
+            ->orderBy(['id' => SORT_DESC])
+            ->one();
+
+        [$job, $translations] = $I->createJobWithTranslations([
             'title' => 'Awesome test job',
-            'elementIds' => [999],
+            'elementIds' => [$element->id],
             'targetSiteIds' => '*',
             'sourceSiteId' => Craftliltplugin::getInstance()->languageMapper->getSiteIdByLanguage('en-US'),
             'translationWorkflow' => SettingsResponse::LILT_TRANSLATION_WORKFLOW_VERIFIED,
@@ -60,10 +77,27 @@ class FetchJobStatusFromConnectorCest extends AbstractIntegrationCest
 
         $totalJobs = Craft::$app->queue->getJobInfo();
 
-        Assert::assertCount(1, $totalJobs);
+        Assert::assertCount(3, $totalJobs);
         $I->assertJobInQueue(
-            new FetchVerifiedJobTranslationsFromConnector([
+            new FetchTranslationFromConnector([
                 'jobId' => $job->id,
+                'translationId' => $translations[0]->id,
+                'liltJobId' => 777
+            ])
+        );
+
+        $I->assertJobInQueue(
+            new FetchTranslationFromConnector([
+                'jobId' => $job->id,
+                'translationId' => $translations[1]->id,
+                'liltJobId' => 777
+            ])
+        );
+
+        $I->assertJobInQueue(
+            new FetchTranslationFromConnector([
+                'jobId' => $job->id,
+                'translationId' => $translations[2]->id,
                 'liltJobId' => 777
             ])
         );
@@ -80,9 +114,14 @@ class FetchJobStatusFromConnectorCest extends AbstractIntegrationCest
         $user = Craft::$app->getUsers()->getUserById(1);
         $I->amLoggedInAs($user);
 
-        $job = $I->createJob([
+        $element = Entry::find()
+            ->where(['authorId' => 1])
+            ->orderBy(['id' => SORT_DESC])
+            ->one();
+
+        [$job, $translations] = $I->createJobWithTranslations([
             'title' => 'Awesome test job',
-            'elementIds' => [999],
+            'elementIds' => [ $element->id ],
             'targetSiteIds' => '*',
             'sourceSiteId' => Craftliltplugin::getInstance()->languageMapper->getSiteIdByLanguage('en-US'),
             'translationWorkflow' => SettingsResponse::LILT_TRANSLATION_WORKFLOW_INSTANT,
@@ -109,10 +148,27 @@ class FetchJobStatusFromConnectorCest extends AbstractIntegrationCest
 
         $totalJobs = Craft::$app->queue->getJobInfo();
 
-        Assert::assertCount(1, $totalJobs);
+        Assert::assertCount(3, $totalJobs);
         $I->assertJobInQueue(
-            new FetchInstantJobTranslationsFromConnector([
+            new FetchTranslationFromConnector([
                 'jobId' => $job->id,
+                'translationId' => $translations[0]->id,
+                'liltJobId' => 777
+            ])
+        );
+
+        $I->assertJobInQueue(
+            new FetchTranslationFromConnector([
+                'jobId' => $job->id,
+                'translationId' => $translations[1]->id,
+                'liltJobId' => 777
+            ])
+        );
+
+        $I->assertJobInQueue(
+            new FetchTranslationFromConnector([
+                'jobId' => $job->id,
+                'translationId' => $translations[2]->id,
                 'liltJobId' => 777
             ])
         );
