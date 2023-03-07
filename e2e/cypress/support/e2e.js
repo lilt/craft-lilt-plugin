1bcf56cf4ad72aa755d9f043eead4c15d4f49ffd
Volodymyr Hadomskyi <volodymyr.hadomskyi@outlook.com>
2023-03-07 22:40:34 +0100

    [E2E] Disable fail on JS errors

--- e2e/cypress/support/e2e.js
+++ e2e/cypress/support/e2e.js
@@ -5,6 +5,11 @@ import './flow/verified'
 beforeEach(() => {
   cy.wrap(Cypress.session.clearAllSavedSessions())
 
+  // Disable fail on JS errors
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+  })
+
   const appUrl = Cypress.env('APP_URL');
   const userName = Cypress.env('USER_NAME');
   const userPassword = Cypress.env('USER_PASSWORD');
