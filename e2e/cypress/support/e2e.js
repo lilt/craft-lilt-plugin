f907f81c4a7712f88a8d6951d91d34fc604ff11c
Volodymyr Hadomskyi <volodymyr.hadomskyi@outlook.com>
2023-03-07 23:28:28 +0100

    [E2E] add on fail script

--- e2e/cypress/support/e2e.js
+++ e2e/cypress/support/e2e.js
@@ -1,29 +1,46 @@
-import './commands'
-import './flow/instant'
-import './flow/verified'
+import './commands';
+import './flow/instant';
+import './flow/verified';
 
 beforeEach(() => {
-  cy.wrap(Cypress.session.clearAllSavedSessions())
+  cy.wrap(Cypress.session.clearAllSavedSessions());
+
+  Cypress.on('fail', (error, runnable) => {
+    // we now have access to the err instance
+    // and the mocha runnable this failed on
+
+    const testName = Cypress.spec.name;
+    const screenshotFileName = `${testName} -- ${runnable.parent.title} -- ${runnable.title} (failed).png`;
+
+    // take a screenshot and save it as a file
+    cy.screenshot(screenshotFileName, {
+      capture: 'fullPage',
+    });
+
+    // add the screenshot to the error stack trace
+    error.stack += `\n\nScreenshot: ${Cypress.config(
+        'screenshotsFolder')}/${screenshotFileName}\n`;
+
+    // throw the error again so that it fails the test
+    throw error;
+  });
 
   // Disable fail on JS errors
   Cypress.on('uncaught:exception', (err, runnable) => {
-    return false
-  })
+    return false;
+  });
 
   const appUrl = Cypress.env('APP_URL');
   const userName = Cypress.env('USER_NAME');
   const userPassword = Cypress.env('USER_PASSWORD');
 
-  cy.visit(`${appUrl}/admin/login`)
+  cy.visit(`${appUrl}/admin/login`);
 
-  cy.get('#loginName')
-  .type(userName)
+  cy.get('#loginName').type(userName);
 
-  cy.get('#password')
-  .type(userPassword)
+  cy.get('#password').type(userPassword);
 
-  cy.get('#submit')
-  .click()
+  cy.get('#submit').click();
 
-  cy.url().should('contain', '/admin/dashboard')
-})
+  cy.url().should('contain', '/admin/dashboard');
+});
