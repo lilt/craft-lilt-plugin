eb9652a8982a3fb416ae76d654e1b569ca148e12
Volodymyr Hadomskyi <volodymyr.hadomskyi@outlook.com>
2023-03-09 17:39:15 +0100

    [E2E] split all files, update composer

---
+++ e2e/cypress/e2e/jobs/verified/success-path-multiple-single-publishing-3.cy.js
@@ -0,0 +1,24 @@
+const {generateJobData} = require('../../../support/job/generator.js');
+
+describe(
+    '[Verified] Success path for job with multiple target languages with single publishing',
+    () => {
+      const entryLabel = 'The Future of Augmented Reality';
+      const entryId = 24;
+
+      it('with copy slug enabled & enable after publish disabled', () => {
+        const {jobTitle, slug} = generateJobData();
+
+        cy.verifiedFlow({
+          slug,
+          entryLabel,
+          jobTitle,
+          entryId,
+          copySlug: true,
+          enableAfterPublish: false,
+          languages: ['de', 'es', 'uk'],
+          batchPublishing: false,
+        });
+      });
+
+    });
