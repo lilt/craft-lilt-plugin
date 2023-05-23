6e65be0ae5cd3db3351dba794b92873266ee9e85
Volodymyr Hadomskyi <volodymyr.hadomskyi@outlook.com>
2023-05-23 19:24:38 +0200

    Hardcode yii on 2.0.47

--- craft-versions.sh
+++ craft-versions.sh
@@ -4,6 +4,7 @@ versions=("$@")
 
 for i in "${versions[@]}"; do
   composer require craftcms/cms:"$i" -W
+  composer require yiisoft/yii2:"2.0.47" -W
   composer dump-autoload
   php vendor/bin/codecept build
 
