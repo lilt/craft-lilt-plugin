90e13f0e8cff4b266211634d8119492180c5d77f
Nathan Lucas <nathan.lucas@lilt.com>
2025-09-23 15:48:44 -0400

    update declaration of TestErrorController

--- tests/integration/controllers/PluginControllerCest.php
+++ tests/integration/controllers/PluginControllerCest.php
@@ -6,36 +6,32 @@ namespace lilthq\craftliltplugin\controllers {
     use yii\web\Response;
     use RuntimeException;
 
-    class TestErrorController extends PluginController
-    {
-        protected array|int|bool $allowAnonymous = true;
-
-        public function beforeAction($action): bool
+    if (!class_exists(TestErrorController::class)) {
+        class TestErrorController extends PluginController
         {
-            if ($action->id === 'error-in-before-action') {
-                throw new RuntimeException('This is a test beforeAction error.');
+            protected array|int|bool $allowAnonymous = true;
+
+            public function beforeAction($action): bool
+            {
+                if ($action->id === 'error-in-before-action') {
+                    throw new RuntimeException('This is a test beforeAction error.');
+                }
+                return parent::beforeAction($action);
             }
-            return parent::beforeAction($action);
-        }
 
-        public function actionErrorInRunAction(): Response
-        {
-            throw new RuntimeException('This is a test runAction error.');
+            public function actionErrorInRunAction(): Response
+            {
+                throw new RuntimeException('This is a test runAction error.');
+            }
         }
     }
 }
 
-// This is the actual test suite namespace.
 namespace lilthq\craftliltplugintests\integration\controllers {
-
     use Craft;
-    use Exception;
     use IntegrationTester;
     use lilthq\craftliltplugin\controllers\TestErrorController;
-    use lilthq\craftliltplugin\Craftliltplugin;
-    use lilthq\craftliltplugin\services\LogsApi;
     use lilthq\craftliltplugintests\integration\AbstractIntegrationCest;
-    use PHPUnit\Framework\MockObject\MockObject;
 
     class PluginControllerCest extends AbstractIntegrationCest
     {
@@ -49,16 +45,15 @@ namespace lilthq\craftliltplugintests\integration\controllers {
         {
             parent::_after($I);
             unset(Craft::$app->controllerMap['test-error']);
-            Craftliltplugin::setInstance(null);
-            Craft::$app->set('craftliltplugin', null);
         }
 
         public function testRunActionCatchesError(IntegrationTester $I): void
         {
-            /** @var MockObject|LogsApi $logsApiMock */
-            $logsApiMock = $this->createMock(LogsApi::class);
-            $logsApiMock->expects($I->once())->method('postLog');
-            $this->mockPluginService($logsApiMock);
+            $I->expectLogPostRequest(
+                '/api/v1.0/logs',
+                'This is a test runAction error.',
+                200
+            );
 
             $I->sendAjaxPostRequest('index.php?action=test-error/error-in-run-action');
 
@@ -72,15 +67,11 @@ namespace lilthq\craftliltplugintests\integration\controllers {
 
         public function testBeforeActionCatchesError(IntegrationTester $I): void
         {
-            /** @var MockObject|LogsApi $logsApiMock */
-            $logsApiMock = $this->createMock(LogsApi::class);
-            $logsApiMock->expects($I->once())
-                ->method('postLog')
-                ->with(
-                    $I->equalTo('error-in-before-action'),
-                    $I->equalTo('This is a test beforeAction error.')
-                );
-            $this->mockPluginService($logsApiMock);
+            $I->expectLogPostRequest(
+                '/api/v1.0/logs',
+                'This is a test beforeAction error.',
+                200
+            );
 
             $I->sendAjaxPostRequest('index.php?action=test-error/error-in-before-action');
 
@@ -89,24 +80,16 @@ namespace lilthq\craftliltplugintests\integration\controllers {
 
         public function testApiLoggingFailureDoesNotAffectUserResponse(IntegrationTester $I): void
         {
-            /** @var MockObject|LogsApi $logsApiMock */
-            $logsApiMock = $this->createMock(LogsApi::class);
-            $logsApiMock->expects($I->once())
-                ->method('postLog')
-                ->willThrowException(new Exception('Remote API is down.'));
-            $this->mockPluginService($logsApiMock);
+            $I->expectLogPostRequest(
+                '/api/v1.0/logs',
+                'This is a test runAction error.',
+                500
+            );
 
             $I->sendAjaxPostRequest('index.php?action=test-error/error-in-run-action');
 
             $I->seeResponseCodeIs(200);
             $I->seeResponseContainsJson(['message' => 'This is a test runAction error.']);
-        }
-
-        private function mockPluginService(LogsApi|MockObject $logsApiMock): void
-        {
-            $pluginMock = $this->createMock(Craftliltplugin::class);
-            $pluginMock->logsApi = $logsApiMock;
-            Craftliltplugin::setInstance($pluginMock);
         }
     }
 }
