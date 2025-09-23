7a15c32d73e5fb98d50566cf95d4a0ed9eee2419
Nathan Lucas <nathan.lucas@lilt.com>
2025-09-23 15:39:06 -0400

    move to WireMocks

--- tests/integration/controllers/job/PostCreateJobControllerCest.php
+++ tests/integration/controllers/job/PostCreateJobControllerCest.php
@@ -2,98 +2,95 @@
 
 declare(strict_types=1);
 
-namespace lilthq\craftliltplugintests\integration\controllers\job;
+namespace lilthq\craftliltplugin\controllers {
 
-use Codeception\Exception\ModuleException;
-use Craft;
-use craft\elements\Entry;
-use craft\errors\MissingComponentException;
-use IntegrationTester;
-use LiltConnectorSDK\Model\SettingsResponse;
-use lilthq\craftliltplugin\controllers\job\PostCreateJobController;
-use lilthq\craftliltplugin\Craftliltplugin;
-use lilthq\craftliltplugin\elements\Job;
-use lilthq\craftliltplugintests\integration\AbstractIntegrationCest;
-use lilthq\tests\fixtures\EntriesFixture;
-use PHPUnit\Framework\Assert;
-use yii\base\InvalidConfigException;
-use yii\db\Exception;
+    use RuntimeException;
+    use yii\web\Response;
 
-class PostCreateJobControllerCest extends AbstractIntegrationCest
-{
-    public function _fixtures(): array
+    class TestErrorController extends PluginController
     {
-        return [
-            'entries' => [
-                'class' => EntriesFixture::class,
-            ]
-        ];
-    }
+        protected array|int|bool $allowAnonymous = true;
 
-    /**
-     * @throws InvalidConfigException
-     */
-    private function getController(): PostCreateJobController
-    {
-        Craftliltplugin::getInstance()->controllerNamespace = 'lilthq\craftliltplugin\controllers';
-        return Craft::$app->createController('craft-lilt-plugin/job/post-create-job/invoke')[0];
-    }
+        public function beforeAction($action): bool
+        {
+            if ($action->id === 'error-in-before-action') {
+                throw new RuntimeException('This is a test beforeAction error.');
+            }
+            return parent::beforeAction($action);
+        }
 
-    /**
-     * @throws MissingComponentException
-     * @throws InvalidConfigException
-     * @throws Exception|ModuleException
-     */
-    public function testCreateJob(IntegrationTester $I): void
-    {
-        $element = Entry::findOne(['authorId' => 1]);
-
-        $user = Craft::$app->getUsers()->getUserById(1);
-        $I->amLoggedInAs($user);
-
-        $I->sendAjaxPostRequest(
-            '?p=admin/craft-lilt-plugin/job/create',
-            [
-                'csrf' => Craft::$app->getRequest()->getCsrfToken(true),
-                'entries' => json_encode([$element->id]),
-                'title' => 'Awesome test job',
-                'sourceSite' => Craftliltplugin::getInstance()->languageMapper->getSiteIdByLanguage('en-US'),
-                'translationWorkflow' => strtolower(SettingsResponse::LILT_TRANSLATION_WORKFLOW_INSTANT),
-                'targetSiteIds' => '*',
-            ]
-        );
-        $I->seeResponseCodeIs(302);
-
-        $job = Job::find()
-            ->orderBy(['id' => SORT_DESC])
-            ->one();
-
-        $I->seeHeader(
-            'x-redirect',
-            sprintf('https://test.craftcms.test:80/index.php?p=admin/craft-lilt-plugin/job/edit/%d&site=default', $job->id)
-        );
-
-        Assert::assertSame('Awesome test job', $job->title);
-        Assert::assertSame([$element->id], $job->getElementIds());
-        Assert::assertSame('instant', $job->translationWorkflow);
-        Assert::assertSame(
-            Craftliltplugin::getInstance()->languageMapper->getSiteIdByLanguage('en-US'),
-            $job->sourceSiteId
-        );
-        Assert::assertSame(
-            'en-US',
-            $job->sourceSiteLanguage
-        );
-
-        Assert::assertCount(3, $job->getTargetSiteIds());
-
-        Assert::assertEquals(
-            ['de-DE', 'es-ES', 'ru-RU'],
-            Craftliltplugin::getInstance()
-                ->languageMapper
-                ->getLanguagesBySiteIds(
-                    $job->getTargetSiteIds()
-                )
-        );
+        public function actionErrorInRunAction(): Response
+        {
+            throw new RuntimeException('This is a test runAction error.');
+        }
     }
 }
+
+namespace lilthq\craftliltplugintests\integration\controllers {
+
+    use Craft;
+    use IntegrationTester;
+    use lilthq\craftliltplugin\controllers\TestErrorController;
+    use lilthq\craftliltplugintests\integration\AbstractIntegrationCest;
+
+    class PluginControllerCest extends AbstractIntegrationCest
+    {
+        public function _before(IntegrationTester $I): void
+        {
+            parent::_before($I);
+            Craft::$app->controllerMap['test-error'] = TestErrorController::class;
+        }
+
+        public function _after(IntegrationTester $I): void
+        {
+            parent::_after($I);
+            unset(Craft::$app->controllerMap['test-error']);
+        }
+
+        public function testRunActionCatchesError(IntegrationTester $I): void
+        {
+            $I->expectLogPostRequest(
+                '/api/v1.0/logs',
+                'This is a test runAction error.',
+                200
+            );
+
+            $I->sendAjaxPostRequest('index.php?action=test-error/error-in-run-action');
+
+            $I->seeResponseCodeIs(200);
+            $I->seeResponseIsJson();
+            $I->seeResponseContainsJson([
+                'success' => false,
+                'message' => 'This is a test runAction error.',
+            ]);
+        }
+
+        public function testBeforeActionCatchesError(IntegrationTester $I): void
+        {
+            $I->expectLogPostRequest(
+                '/api/v1.0/logs',
+                'This is a test beforeAction error.',
+                200
+            );
+
+            $I->sendAjaxPostRequest('index.php?action=test-error/error-in-before-action');
+
+            $I->seeResponseCodeIs(404);
+        }
+
+        public function testApiLoggingFailureDoesNotAffectUserResponse(IntegrationTester $I): void
+        {
+            $I->expectLogPostRequest(
+                '/api/v1.0/logs',
+                'This is a test runAction error.',
+                500
+            );
+
+            $I->sendAjaxPostRequest('index.php?action=test-error/error-in-run-action');
+
+            $I->seeResponseCodeIs(200);
+            $I->seeResponseContainsJson(['message' => 'This is a test runAction error.']);
+        }
+    }
+}
+
